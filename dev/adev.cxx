d3e5a461afa1495b5675e0de0d543c6294f71e14
Mike <mike@ThinkpadX1.home>
2024-01-22 17:06:57 +0100

    repo update

--- dev/adev.cxx
+++ dev/adev.cxx
@@ -28,6 +28,29 @@
 #include "../inc/toolbox.hxx"
 using namespace std;
 
+void legendre(vector<PrimePower>&pp_in, ul n, vector<PrimePower>&pp_out);
+
+void legendre(vector<PrimePower>&pp_in, ul n, vector<PrimePower>&pp_out){
+	//~ Outline:
+	//~ declare ul sum
+	//~ for each prime in pp_in
+		//~ clear sum
+		//~ Sum Floor n/p^1,2,3 etc
+		//~ pp.push_back(make_pair(prime,sum))
+	//~ stop
+	ul sum, divisor, r;
+	pp_out.clear();
+	for(auto pp : pp_in){
+		sum = 0;
+		divisor = pp.first;
+		do {
+			r = floor(n/divisor);	//floor imported from toolbox.hxx
+			divisor *= pp.first;
+			sum += r;			
+		} while (r != 0);
+		pp_out.push_back(make_pair(pp.first, sum));
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -35,12 +58,25 @@ int main(int argc, char **argv)
 	vector<ul>primes;
 	SieveOfEratosthenes(primes, 1001);
 	PfactOfN pf_n;
-	generate_descriptors(primes, 138, pf_n);
+	
+	//~ generate_descriptors(primes, 138, pf_n);
+	//~ for(auto pp : pf_n) cout << pp.first << "^" << pp.second << "  ";
+	//~ cout << endl;
+	vector<PrimePower> pp_out;
+	//~ legendre(pf_n, 138, pp_out);
+	//~ cout << "138 factorial prime/powers" << endl;
+	//~ for(auto pp : pp_out) cout << pp.first << "^" << pp.second << "  ";
+	//~ cout << endl;
+	
+	const ul n=276;
+	generate_descriptors(primes, n, pf_n);
 	for(auto pp : pf_n) cout << pp.first << "^" << pp.second << "  ";
 	cout << endl;
-	generate_descriptors(primes, 171, pf_n);
-	for(auto pp : pf_n) cout << pp.first << "^" << pp.second << "  ";
+	legendre(pf_n, n, pp_out);
+	cout << n << " factorial prime/powers" << endl;
+	for(auto pp : pp_out) cout << pp.first << "^" << pp.second << "  ";
 	cout << endl;
+	
 	return 0;
 }
 
