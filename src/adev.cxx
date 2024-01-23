cafd7410fd639d56a56f0ec4bfac30554c0414a2
Mike <Mike@Aorus_F39>
2024-01-23 12:02:09 +0100

    bdev provides a consistent result for 138^1234567890. The minimum value of n, for which n! is divisible by (138!)^1234567890 is 138*1234567890

--- src/adev.cxx
+++ src/adev.cxx
@@ -1,7 +1,7 @@
 /*
- * bdev.cxx
+ * adev.cxx
  * 
- * Copyright 2024 Mike <mike@Aorus39>
+ * Copyright 2024 Mike <mike@fedora38-2.home>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,6 +21,7 @@
  * 
  */
 
+
 #include <iostream>
 #include <vector>
 #include <utility>
@@ -28,16 +29,22 @@
 using namespace std;
 
 void legendre(vector<PrimePower>&pp_in, ul n, vector<PrimePower>&pp_out);
-bool num_isdivisible_den(vector<PrimePower> num, vector<PrimePower> den);
 
 void legendre(vector<PrimePower>&pp_in, ul n, vector<PrimePower>&pp_out){
+	//~ Outline:
+	//~ declare ul sum
+	//~ for each prime in pp_in
+		//~ clear sum
+		//~ Sum Floor n/p^1,2,3 etc
+		//~ pp.push_back(make_pair(prime,sum))
+	//~ stop
 	ul sum, divisor, r;
 	pp_out.clear();
 	for(auto pp : pp_in){
 		sum = 0;
 		divisor = pp.first;
 		do {
-			r = floor(n/divisor);	//floor imported via toolbox.hxx
+			r = floor(n/divisor);	//floor imported from toolbox.hxx
 			divisor *= pp.first;
 			sum += r;			
 		} while (r != 0);
@@ -45,52 +52,30 @@ void legendre(vector<PrimePower>&pp_in, ul n, vector<PrimePower>&pp_out){
 	}
 }
 
-bool is_divisible(vector<PrimePower> num, vector<PrimePower> den){
-	vector<PrimePower>::iterator inum, iden;
-	iden = den.begin();
-	while(iden != den.end()){ // Outer loop
-		inum = num.begin();
-		while((inum != num.end())and(inum->first != iden->first)) ++inum;
-		if (inum == num.end()) return false;
-		if (inum->second < iden->second) return false;
-		// check next prime in denominator
-		++iden;
-	}
-	return true;
-		
-	
-	
-	return true;
-}
-
 int main(int argc, char **argv)
 {
 	const ul modulus = 1e18;
-	vector<ul>primes;	// Required by Sieve
+	vector<ul>primes;
 	SieveOfEratosthenes(primes, 1001);
+	PfactOfN pf_n;
 	
-	vector<PrimePower> pf_n, pp_out;
-	vector<PrimePower> num, den;
-
-	ul n=138;
-	ul power=1234567890;
-	generate_descriptors(primes, n, pf_n);	
+	//~ generate_descriptors(primes, 138, pf_n);
+	//~ for(auto pp : pf_n) cout << pp.first << "^" << pp.second << "  ";
+	//~ cout << endl;
+	vector<PrimePower> pp_out;
+	//~ legendre(pf_n, 138, pp_out);
+	//~ cout << "138 factorial prime/powers" << endl;
+	//~ for(auto pp : pp_out) cout << pp.first << "^" << pp.second << "  ";
+	//~ cout << endl;
+	
+	const ul n=276;
+	generate_descriptors(primes, n, pf_n);
+	for(auto pp : pf_n) cout << pp.first << "^" << pp.second << "  ";
+	cout << endl;
 	legendre(pf_n, n, pp_out);
-	den = pp_out;
-	// Raise demoninator powers
-	for(auto pp = den.begin(); pp != den.end(); ++pp){
-		pp->second = pp->second * power;
-	}
-	// Find a value for where n! is divisible by den.
-	for(n = (138*power)-1; n != (138*power)+2; ++n){
-		//cout << n << " ";
-		generate_descriptors(primes, n, pf_n);
-		legendre(pf_n, n, num);
-		if(is_divisible(num, den)) {
-			cout << endl << n << "! is divisible." << endl;
-			break;
-		}
-	}		
+	cout << n << " factorial prime/powers" << endl;
+	for(auto pp : pp_out) cout << pp.first << "^" << pp.second << "  ";
+	cout << endl;
 	
 	return 0;
 }
